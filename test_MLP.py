14633a35df42a9942a2d2d472a3fba486f64028f
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-04-14 17:11:02 +0200

    Add layer

--- test_MLP.py
+++ test_MLP.py
@@ -19,15 +19,16 @@ train_input = train_input.view(316,1400).squeeze(1)
 test_input = test_input.view(100,1400).squeeze(1)
 
 class Net(nn.Module):
-    def __init__(self, hidden):
+    def __init__(self, hidden1, hidden2):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(1400, hidden)
-        self.fc2 = nn.Linear(hidden, 2)
+        self.fc1 = nn.Linear(1400, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc3 = nn.Linear(hidden2, 2)
 
     def forward(self, x):
-        x = self.fc1(x)
-        # x = F.tanh(self.fc2(x))
-        x = F.sigmoid(self.fc2(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.sigmoid(self.fc3(x))
         return x
 
 def train_model(model, train_input, train_target, mini_batch_size):
@@ -36,9 +37,9 @@ def train_model(model, train_input, train_target, mini_batch_size):
         # We do this with mini-batches
         for b in range(0, train_input.size(0), mini_batch_size):
             output = model.forward(train_input.narrow(0, b, mini_batch_size))
-            fewf = train_target.narrow(0, b, mini_batch_size).long()
+            train_target_narrowed = train_target.narrow(0, b, mini_batch_size).long()
 
-            loss = criterion(output, fewf)
+            loss = criterion(output, train_target_narrowed)
             sum_loss = sum_loss + loss.data[0]
             model.zero_grad()
             loss.backward()
@@ -58,17 +59,16 @@ def compute_nb_errors(model, input, target, mini_batch_size):
 train_input, train_target = Variable(train_input), Variable(train_target)
 test_input, test_target = Variable(test_input), Variable(test_target)
 
-# hiddens = [10, 50, 200, 500]
-hiddens = [50]
-for i in hiddens:
-    print("Hidden layer's dimension = {0:d}".format(i))
-    # model, criterion = Net(i), nn.MSELoss()
-    model, criterion = Net(i), nn.CrossEntropyLoss()
-    eta, mini_batch_size = 1e-1, 79
+hidden1 = 50
+hidden2 = 50
 
-    train_model(model, train_input, train_target, mini_batch_size)
-    nberrors_train = compute_nb_errors(model,train_input, train_target, mini_batch_size)
-    nberrors_test = compute_nb_errors(model,test_input, test_target, mini_batch_size)
+# model, criterion = Net(hidden1), nn.MSELoss()
+model, criterion = Net(hidden1,hidden2), nn.CrossEntropyLoss()
+eta, mini_batch_size = 1e-1, 79
 
-    print("Train error: {0:.2f}%".format((nberrors_train/train_size)*100))
-    print("Test error: {0:.2f}%".format((nberrors_test/test_size)*100))
+train_model(model, train_input, train_target, mini_batch_size)
+nberrors_train = compute_nb_errors(model,train_input, train_target, mini_batch_size)
+nberrors_test = compute_nb_errors(model,test_input, test_target, mini_batch_size)
+
+print("Train error: {0:.2f}%".format((nberrors_train/train_size)*100))
+print("Test error: {0:.2f}%".format((nberrors_test/test_size)*100))
