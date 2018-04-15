61448e9d38dc7b9dd7a65a396580a329afe8b1cd
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-04-15 20:49:21 +0200

    Modify network to combine velocity and acceleration

--- test_MLP.py
+++ test_MLP.py
@@ -24,16 +24,43 @@ test_size = test_input.size(0)
 class Net(nn.Module):
     def __init__(self,hidden):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv1d(28, 32, kernel_size=6)
-        self.conv2 = nn.Conv1d(32, 64, kernel_size=4)
-        self.fc1 = nn.Linear(256, hidden)
-        self.fc2 = nn.Linear(hidden, 2)
+        self.conv1 = nn.Conv1d(28, 28, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv1d(28, 28, kernel_size=3, padding=1)
+        self.fc_pos = nn.Linear(1400, 40);
+        self.fc_vel = nn.Linear(1400, 40)
+        self.fc_acc = nn.Linear(1400, 40)
+        self.fc_sum = nn.Linear(40, 28)
+        self.fc_final = nn.Linear(28,2)
+
+    def forward(self, x):
+        v = F.relu(F.max_pool1d(self.conv1(x), kernel_size=1, stride=1))
+        a = F.relu(F.max_pool1d(self.conv2(v), kernel_size=1, stride=1))
+
+        x = F.relu(self.fc_pos(x.view(-1, 1400)))
+        v = F.relu(self.fc_vel(v.view(-1, 1400)))
+        a = F.relu(self.fc_acc(a.view(-1, 1400)))
+
+        sum = x + v + a
+        sum = F.sigmoid(self.fc_sum(sum))
+
+        res = self.fc_final(sum)
+        return res
+
+class Net2(nn.Module):
+    def __init__(self,hidden1,hidden2):
+        super(Net2, self).__init__()
+        self.conv1 = nn.Conv1d(28, 32, kernel_size=3)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(448, 100)
+        self.fc3 = nn.Linear(hidden2, 2)
 
     def forward(self, x):
         x = F.relu(F.max_pool1d(self.conv1(x), kernel_size=3, stride=3))
-        x = F.relu(F.max_pool1d(self.conv2(x), kernel_size=3, stride=3))
-        x = F.relu(self.fc1(x.view(-1, 256)))
+        x = F.relu(F.max_pool1d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool1d(self.conv3(x), kernel_size=5, stride=5))
+        x = F.relu(self.fc1(x.view(-1, 128)))
         x = self.fc2(x)
+        x = self.fc3(x)
         return x
 
 def compute_nb_errors(model, input, target):
@@ -50,7 +77,7 @@ def train_model(model, train_input, train_target, validation_input, validation_t
     train_size = train_input.size(0)
     validation_size = validation_input.size()
 
-    n_epochs = 1000
+    n_epochs = 200
 
     for e in range(0, n_epochs):
         mini_batch_size = initial_mini_batch_size
@@ -100,15 +127,16 @@ test_input, test_target = Variable(test_input), Variable(test_target)
 
 train_input, train_target, validation_input, validation_output = create_validation(train_input, train_target, 0.2)
 
-hidden = 100
+hidden1 = 100
+hidden2 = 100
 # model, criterion = Net(hidden1), nn.MSELoss()
-model, criterion = Net(hidden), nn.CrossEntropyLoss()
+model, criterion = Net(hidden1), nn.CrossEntropyLoss()
 
 eta, mini_batch_size = 1e-1, 79
 
 train_model(model, train_input, train_target, validation_input, validation_output, mini_batch_size)
-nberrors_train = compute_nb_errors(model,train_input, train_target, mini_batch_size)
-nberrors_test = compute_nb_errors(model,test_input, test_target, mini_batch_size)
+nberrors_train = compute_nb_errors(model,train_input, train_target)
+nberrors_test = compute_nb_errors(model,test_input, test_target)
 
 print("Train error: {0:.2f}%".format((nberrors_train/train_size)*100))
 print("Test error: {0:.2f}%".format((nberrors_test/test_size)*100))
