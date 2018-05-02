f483b744055112f70e8136728b9a3c7812d29c9c
Caterina Bigoni <caterinabigoni@tsf-484-wpa-1-218.epfl.ch>
2018-05-02 10:12:01 +0200

    new loader file for data augmentation

---
+++ loader2.py
@@ -0,0 +1,31 @@
+import dlc_bci as bci
+import torch
+import numpy
+
+def load_data(data_aug=False):
+    train_input, train_target = bci.load(root='./data_bci', one_khz=True)
+
+    test_input, test_target = bci.load(root='./data_bci',train=False)
+
+    train_samples = train_input.size(0)
+    test_samples = test_input.size(0)
+    channels = train_input.size(1)
+
+    for i in range(0,channels):
+        me = torch.mean(train_input[:,i,:])
+        std = torch.std(train_input[:,i,:])
+        train_input[:,i,:] = (train_input[:,i,:] - me)/std;
+        test_input[:,i,:] = (test_input[:,i,:] - me)/std;
+
+    if data_aug:
+        train_input_list = []
+        train_target_list = []
+        for i in range(10):
+            train_input_list.append(train_input[:,:,i::10])
+            train_target_list.append(train_target)
+            
+        train_input = torch.cat(train_input_list,dim=0)
+        train_target = torch.cat(train_target_list,dim=0)
+    
+
+    return train_input, train_target, test_input, test_target
