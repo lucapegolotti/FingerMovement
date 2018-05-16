0e0cc10dd39cb79ec9ac8acafed3e3636adf4914
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-05-16 21:20:28 +0200

    Comment models

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from torch.autograd import Variable
 from torch import nn
 from torch.nn import functional as F
 
-class M1(nn.Module): # Linear Predictor
+# Linear Perceptron: a linear perceptron with no activation function
+class M1(nn.Module):
     def __init__(self):
         super(M1, self).__init__()
         self.fc1 = nn.Linear(1400, 2)
@@ -14,7 +15,12 @@ class M1(nn.Module): # Linear Predictor
         x = self.fc1(x)
         return x
 
-class M2(nn.Module): # MC_DCNNNet
+# Simple convolutional neural network: the kernels of the two 1d convolutions are
+# applied in the time timesteps. After each convolution, an average pool is used
+# to reduce the dimensionality of the third dimension of the tensors and relu
+# functions are used as non-linearities. The output of the 2nd convolution enters
+# two linear nodes
+class M2(nn.Module):
     def __init__(self):
         super(M2, self).__init__()
         self.conv1 = nn.Conv1d(28, 30, kernel_size=6)
@@ -29,11 +35,13 @@ class M2(nn.Module): # MC_DCNNNet
         x = self.fc2(x)
         return x
 
-class M3(nn.Module): # M3
+# Multichannel deep convolutional neural network + linear perceptron: the
+# convolutional module applies uni-dimensional kernels (different for each channel)
+# in the direction of the timesteps.
+class M3(nn.Module):
     def __init__(self):
         super(M3, self).__init__()
         self.conv1 = nn.Conv1d(28, 28, kernel_size=6, groups=28, bias=True)
-        self.conv2 = nn.Conv1d(224, 112, kernel_size=4, groups=28, bias=True)
         self.fc1 = nn.Linear(252, 25)
         self.fc2 = nn.Linear(25, 16)
         self.fc4 = nn.Linear(1400, 25)
@@ -49,7 +57,10 @@ class M3(nn.Module): # M3
         x = self.fc3(x)
         return x
 
-class M4(nn.Module): #ShallowConvNetPredictor
+# Shallow convolutional neural network: two convolutions are applied to the input
+# tensor. The first one applies a kernel in the direction of the timesteps,
+# whereas the second one operates in the direction of the channels.
+class M4(nn.Module):
     def __init__(self, n_hidden = 125, kernel_size = 11, n_conv_1 = 15, n_conv_2=15):
         super(M4, self).__init__()
 
@@ -61,16 +72,16 @@ class M4(nn.Module): #ShallowConvNetPredictor
 
     def forward(self, x):
         x = x.unsqueeze(1)
-        x = F.relu(self.conv1(x)) # 316x40x28x46
-        x = F.relu(self.conv2(x)) # 316x40x1x46
-        x = F.max_pool2d(x, kernel_size = (1,2), stride = (1,2))  # 316x40x1x23
-        x = x.view(-1, x.size(1)*x.size(3)) # 316x40x23 = 920
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size = (1,2), stride = (1,2))
+        x = x.view(-1, x.size(1)*x.size(3))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
-
-class M4_dropout(nn.Module): #ShallowConvNetPredictorWithDropout
+# Shallow convolutional neural network, with dropout
+class M4_dropout(nn.Module):
    def __init__(self, n_hidden = 20, kernel_size = 5, n_conv_1 = 40, n_conv_2=40, p_dropout = 0.5):
        super(M4_dropout, self).__init__()
 
@@ -85,13 +96,12 @@ class M4_dropout(nn.Module): #ShallowConvNetPredictorWithDropout
    def forward(self, x):
        x = x.unsqueeze(1)
 
-       x = F.relu(self.conv1(x)) # 316x40x28x46
-       x = F.relu(self.conv2(x)) # 316x40x1x46
-       x = F.max_pool2d(x, kernel_size = (1,2), stride = (1,2))  # 316x40x1x23
-       x = x.view(-1, x.size(1)*x.size(3)) # 316x40x23 = 920
+       x = F.relu(self.conv1(x))
+       x = F.relu(self.conv2(x))
+       x = F.max_pool2d(x, kernel_size = (1,2), stride = (1,2))
+       x = x.view(-1, x.size(1)*x.size(3))
        x = self.dropout(x)
        x = F.relu(self.fc1(x))
        x = self.dropout(x)
        x = self.fc2(x)
        return x
-
